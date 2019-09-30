9fed9ad57aad6b35207eaac763bd0691d148d4bb
Oluwaseun Akogun <seun407@gmail.com>
2019-09-30 11:51:32 +0100

    enabled searchbox and cards interaction

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Cardlist from './Cardlist';
+import SearchBox from './Searchbox'
+import { robots } from './robots';
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render () {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    });
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Cardlist robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App
