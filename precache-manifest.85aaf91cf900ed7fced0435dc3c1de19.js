40b61379b97feee90014ac46e279f349a721eaa0
Oluwaseun Akogun <seun407@gmail.com>
2019-10-15 12:20:58 +0100

    Updates

---
+++ precache-manifest.85aaf91cf900ed7fced0435dc3c1de19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87960eb6dbdfcac660ce9ddc37fdf6c9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "007ca526fc52973b063f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "150f854984b14c516ca5",
+    "url": "/robofriends/static/css/main.d682cd88.chunk.css"
+  },
+  {
+    "revision": "007ca526fc52973b063f",
+    "url": "/robofriends/static/js/2.caf37d44.chunk.js"
+  },
+  {
+    "revision": "150f854984b14c516ca5",
+    "url": "/robofriends/static/js/main.9bc2fc48.chunk.js"
+  },
+  {
+    "revision": "ef7ed2c3e67d53841831",
+    "url": "/robofriends/static/js/runtime-main.ed74653d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
